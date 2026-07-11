7f88756039eeb7a02e14122a8eff0df5543c2c58
Jonas Alaf <122956015+DocStriker@users.noreply.github.com>
2026-07-11 05:31:37 +0000

    Refactor packet capture logic to improve clarity and add detailed event logging

--- analyzer/capture.py
+++ analyzer/capture.py
@@ -1,17 +1,65 @@
-from scapy.all import sniff, get_if_list
+from scapy.all import sniff
 from scapy.layers.inet import IP, TCP
-
-print(get_if_list())
 
 SERVER_PORT = 8443
 
+seen = set()   # <-- Aqui
+
 def callback(pkt):
+
     if IP not in pkt or TCP not in pkt:
         return
 
+    ip = pkt[IP]
     tcp = pkt[TCP]
 
-    if tcp.sport == SERVER_PORT or tcp.dport == SERVER_PORT:
-        print(pkt.summary())
+    if SERVER_PORT not in (tcp.sport, tcp.dport):
+        return
+    
+    print("=" * 60)
 
-sniff(iface='lo',prn=callback)
+    direction = (
+        "CLIENTE → SERVIDOR"
+        if tcp.dport == SERVER_PORT
+        else "SERVIDOR → CLIENTE"
+    )
+
+    key = (
+        ip.src,
+        ip.dst,
+        tcp.sport,
+        tcp.dport,
+        tcp.seq,
+        tcp.ack,
+        str(tcp.flags),
+        len(tcp.payload)
+    )
+
+    if key in seen:
+
+        print(direction)
+        print(f"{ip.src}:{tcp.sport} -> {ip.dst}:{tcp.dport}")
+
+        flags = tcp.flags
+
+        if flags == "S":
+            print("Evento: SYN")
+
+        elif flags == "SA":
+            print("Evento: SYN-ACK")
+
+        elif flags == "A":
+            print("Evento: ACK")
+
+        elif "F" in flags:
+            print("Evento: FIN")
+
+        elif "R" in flags:
+            print("Evento: RST")
+        return
+
+    seen.add(key)
+
+    #print(pkt.summary())
+
+sniff(iface="lo", prn=callback)
